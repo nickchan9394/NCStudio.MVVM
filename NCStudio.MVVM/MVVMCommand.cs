e4c97b389d434fda04512a0edc5d892cfddf4aa6
NickChan9394 <nickchan9394@live.cn>
2018-03-23 10:24:02 +0800

    set up project and add basic classes

---
+++ NCStudio.MVVM/MVVMCommand.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Reflection;
+
+namespace NCStudio.MVVM
+{
+    public class MvvmCommand:ICommand
+    {
+        private Action<object> execute = null;
+
+        private Func<object, bool> canExecute = null;
+
+        private GenericWeakEventManager<PropertyChangedEventArgs> weakPropertyChangedEventListener;
+
+        private GenericWeakEventManager<NotifyCollectionChangedEventArgs> weakCollectionChangedEventListener;
+
+        #region Implement ICommand
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecute != null)
+            {
+                return this.canExecute(parameter);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (this.execute != null)
+            {
+                this.execute(parameter);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void OnCanExecuteChanged()
+        {
+
+            EventHandler handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+        #endregion
+
+        #region Listner
+       
+
+        private void RequeryCanExecuteForProperty(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            OnCanExecuteChanged();
+        }
+
+        private void RequeryCanExecuteForCollection(object sender, NotifyCollectionChangedEventArgs collectionChangedEventArgs)
+        {
+            OnCanExecuteChanged();
+        }
+
+        public MvvmCommand AddListener<TEntity>(INotifyPropertyChanged source, Expression<Func<TEntity, object>> property)
+        {
+            string propertyName = GetPropertyName(property);
+            PropertyChangedEventManager.AddListener(source, weakPropertyChangedEventListener, propertyName);
+            return this;
+        }
+
+        public MvvmCommand AddListener<TEntity>(INotifyCollectionChanged source)
+        {
+            CollectionChangedEventManager.AddListener(source, weakCollectionChangedEventListener);
+            return this;
+        }
+        #endregion
+
+        public MvvmCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+            
+            weakPropertyChangedEventListener = new GenericWeakEventManager<PropertyChangedEventArgs>(RequeryCanExecuteForProperty);
+            weakCollectionChangedEventListener = new GenericWeakEventManager<NotifyCollectionChangedEventArgs>(RequeryCanExecuteForCollection);
+        }
+
+        private String GetPropertyName<TEntity>(Expression<Func<TEntity, object>> expression)
+        {
+            var lambda = expression as LambdaExpression;
+            MemberExpression memberExpression;
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = lambda.Body as UnaryExpression;
+                memberExpression = unaryExpression.Operand as MemberExpression;
+            }
+            else
+            {
+                memberExpression = lambda.Body as MemberExpression;
+            }
+            var constantExpression = memberExpression.Expression as ConstantExpression;
+            var propertyInfo = memberExpression.Member as PropertyInfo;
+
+            return propertyInfo.Name;
+
+        }
+
+
+
+        
+        
+    }
+}
---
+++ NCStudio.MVVM/ObservableObject.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace NCStudio.MVVM
+{
+    public abstract class ObservableObject<TSource>:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged<TProperty>(Expression<Func<TSource,TProperty>> propertyExpression)
+        {
+            String propertyName=GetPropertyName<TProperty>(propertyExpression);
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this,new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void OnPropertyChanged(String propertyName)
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void OnPropertyChanged<TProperty>(Expression<Func<TSource,TProperty>> propertyExpression,Object oldValue,Object newValue)
+        {
+            String propertyName = GetPropertyName<TProperty>(propertyExpression);
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedExtendedEventArgs(propertyName,oldValue,newValue));
+            }
+        }
+
+        public void OnPropertyChanged(String propertyName,Object oldValue,Object newValue)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedExtendedEventArgs(propertyName, oldValue, newValue));
+            }
+        }
+
+        private string GetPropertyName<TProperty>(Expression<Func<TSource, TProperty>> expression)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException("propertyExpression");
+            }
+
+            var memberExpression = expression.Body as MemberExpression;
+
+            if (memberExpression == null)
+            {
+                throw new ArgumentException("The expression is not a member access expression.", "expression");
+            }
+
+            var property = memberExpression.Member as PropertyInfo;
+            if (property == null)
+            {
+                throw new ArgumentException("The member access expression does not access a property.", "expression");
+            }
+
+            var getMethod = property.GetGetMethod(true);
+            if (getMethod.IsStatic)
+            {
+                throw new ArgumentException("The referenced property is a static property.", "expression");
+            }
+
+            return memberExpression.Member.Name;
+        }
+
+        protected virtual bool SetField<TProperty>(ref TProperty field, TProperty value, Expression<Func<TSource, TProperty>> propertyExpression)
+        {
+            if(EqualityComparer<TProperty>.Default.Equals(field,value))return false;
+            field = value;
+            OnPropertyChanged(propertyExpression);
+            return true;
+        }
+    }
+}
